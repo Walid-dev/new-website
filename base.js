af26e915b18f25678fdcaeb97e03dd6800f48e6f
Walid <hlaliwalid@mail.com>
2019-03-04 09:33:05 +0100

    Ajout d'un screen pour le contenu évolutif de l'élément ipad

--- base.js
+++ base.js
@@ -1,8 +1,44 @@
 
+$screen = $('.screen');
+$touchLeftButton =$('#touch-left-button');
 
+$touchLeftButton.prop('disabled' , true);
 
 $(document).ready(function () {
-    $('#main-button-phone').click(function (e) { 
-        $('.screen').toggleClass('active');
+    $('#touch-center-button').click(function (e) { 
+        $screen.toggleClass('active');
+        $touchLeftButton.prop('disabled' , false);
     });
+});
+
+$touchLeftButton.click(function (e) { 
+    e.preventDefault();
+    alert("Hello World");
+});
+
+$('#touch-right-button').click(function (e) { 
+    e.preventDefault();
+    alert("Hello World");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$('.test').click(function (e) { 
+    alert("Hello World");
 });
