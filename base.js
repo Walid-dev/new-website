9c05973b40318fe04504c9c432c64c2b31bf51bc
Walid <hlaliwalid@mail.com>
2019-03-05 13:43:17 +0100

    Fusion de la branche screen-size avec branche principal

--- base.js
+++ base.js
@@ -1,23 +1,159 @@
+// Définition des variables
+
 $centralButton = $('#touch-center-button');
 $leftButton = $('#touch-left-button');
-$rightButton = $('touch-right-button');
-
+$rightButton = $('#touch-right-button');
 $screen = $('.screen');
+$newChildHidden = $('.screen-content-hidden');
+$screenBasicChild = $('.screen-content-child');
+$imageFondScreen = $('#image-fond-screen');
 
 
-$('.phone-hour').hide();
+// Contenu masqué écran off
 
+$('#screen-content-child').hide();
+$('#screen-content-child1').hide();
+$('#screen-content-child2').hide();
+$('#screen-content-child3').hide();
+$('#screen-content-child4').hide();
+$('#screen-content-child5').hide();
+$('#screen-content-child6').hide();
+$('#screen-content-child7').hide();
+$('#screen-content-child7').hide();
+$('#screen-content-child8').hide();
+$imageFondScreen.hide();
+
+
+// Propriétés bouton Tablette
+
+$leftButton.prop( "disabled", true );
+
+
+// Bouton central on Tablette
 
 for(let i = 0 ; i <= 1000 ; i++) {
-    $centralButton.click(function () {
-    if ( (i % 2) == 0) {
-        $('#touch-center-button').css('color' , 'red');
-        $('.phone-hour').show();
-        i++;
-} else if((i % 2) == 1) {
-    $('#touch-center-button').css('color' , 'blue');
-    $('.phone-hour').hide();
-    i++;
-}
+$centralButton.click(function () {
+        if ( (i % 2) == 0) {
+                $centralButton.css('color' , 'red');
+                $screenBasicChild.show();
+                $imageFondScreen.show();
+                $leftButton.prop( "disabled", false );
+                i++;
+        } else if((i % 2) == 1) {
+                $newChildHidden.hide();
+                $screenBasicChild.hide();
+                $imageFondScreen.hide();
+                $centralButton.css('color' , 'blue');
+                $leftButton.css('color' , 'unset');
+                $leftButton.prop( "disabled", true );
+                $rightButton.css('color' , 'unset');
+                i++;
+        }
 });
 }; 
+
+
+// Boutons ouverture Applications
+
+
+$('#btn-icon1').click(function (e) { 
+        e.preventDefault();
+        $screenBasicChild.hide();
+        $('#screen-content-child1').show();
+        $leftButton.css('color' , 'lime');
+});
+
+$('#btn-icon2').click(function (e) { 
+        e.preventDefault();
+        $screenBasicChild.hide();
+        $('#screen-content-child2').show();
+        $leftButton.css('color' , 'lime');
+});
+
+$('#btn-icon3').click(function (e) { 
+        e.preventDefault();
+        $screenBasicChild.hide();
+        $('#screen-content-child3').show();
+        $leftButton.css('color' , 'lime');
+});
+
+$('#btn-icon4').click(function (e) { 
+        e.preventDefault();
+        $screenBasicChild.hide();
+        $('#screen-content-child4').show();
+        $leftButton.css('color' , 'lime');
+});
+
+$('#btn-icon5').click(function (e) { 
+        e.preventDefault();
+        $screenBasicChild.hide();
+        $('#screen-content-child5').show();
+        $leftButton.css('color' , 'lime');
+});
+
+$('#btn-icon6').click(function (e) { 
+        e.preventDefault();
+        $screenBasicChild.hide();
+        $('#screen-content-child6').show();
+        $leftButton.css('color' , 'lime');
+});
+
+$('#btn-icon7').click(function (e) { 
+        e.preventDefault();
+        $screenBasicChild.hide();
+        $('#screen-content-child7').show();
+        $leftButton.css('color' , 'lime');
+});
+
+$('#btn-icon8').click(function (e) { 
+        e.preventDefault();
+        $screenBasicChild.hide();
+        $('#screen-content-child8').show();
+        $leftButton.css('color' , 'lime');
+});
+
+
+// Bouton retour
+
+$leftButton.click(function (e) { 
+        e.preventDefault();
+        $newChildHidden.hide();
+        $screenBasicChild.show();
+        $leftButton.css('color' , 'unset');               
+});
+
+
+//..
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Mise à jour Ajax partie Slider.html
+
+// $('#maj-slider-btn').click(function() {
+//       $('#screen-content-child').load('test.html', function() {             
+//      });
+//      });
+
+// $leftButton.click(function() {
+//       $('#screen-content-child').load('main.html', function() {   
+//               $('#maj-slider-btn').click(function() {
+//                       $('#screen-content-child').load('test.html', function() {             
+//                      });
+//                      });
+//      });
+//      });
